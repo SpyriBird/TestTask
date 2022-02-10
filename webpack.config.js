e3e371509b08139c096b6f3987c509bc58cd1e5d
Tanya <t.goreva@g.nsu.ru>
2022-02-10 23:29:31 +0700

    installed webpack and its cli, dev-server, plugins, loaders

---
+++ webpack.config.js
@@ -0,0 +1,75 @@
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPugPlugin = require('html-webpack-pug-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    mode: 'development',
+    entry: {
+        index: [
+            '/index.pug',
+            '/index.js'
+        ]
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all'
+        }
+    },
+    devServer: {
+        open: true,
+        port: 4200,
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            inject: 'body',
+            filename: 'index.html',
+            minify: false,
+            template: 'index.pug',
+            chunks: ['index']
+        }),
+        new HtmlWebpackPugPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        }),
+    ],
+    module: {
+        rules: [{
+                test: /\.(png|svg|jpg|jpeg|gif|ttf|woff|ico)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'static/[hash][ext][query]'
+                }
+            },
+            {
+                test: /\.js.map$/,
+                use: ["source-map-loader"],
+            },
+            {
+                test: /\.pug$/,
+                loader: 'pug-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    }
+}
